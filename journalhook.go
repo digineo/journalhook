7ad6089eaad47d1681084e6986bdcbb14fcb1e3a
Grigory Zubankov <Grigory.Zubankov@acronis.com>
2017-08-25 18:46:53 +0300

    ssgreg/journald instead of coreos/go-systemd/journal

--- journalhook.go
+++ journalhook.go
@@ -4,36 +4,67 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"unicode"
 
-	"github.com/coreos/go-systemd/journal"
-	logrus "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
+	journald "github.com/ssgreg/journald-send"
 )
 
-type JournalHook struct{}
+// JournalHook is the systemd-journald hook for logrus.
+type JournalHook struct {
+}
 
-var (
-	severityMap = map[logrus.Level]journal.Priority{
-		logrus.DebugLevel: journal.PriDebug,
-		logrus.InfoLevel:  journal.PriInfo,
-		logrus.WarnLevel:  journal.PriWarning,
-		logrus.ErrorLevel: journal.PriErr,
-		logrus.FatalLevel: journal.PriCrit,
-		logrus.PanicLevel: journal.PriEmerg,
+// Fire writes a message to the systemd journal.
+func (h *JournalHook) Fire(entry *logrus.Entry) error {
+	return journald.Send(
+		entry.Message,
+		mapLevelToPriority(entry.Level),
+		// Journal wants uppercase strings.
+		stringifyEntries(entry.Data))
+}
+
+// Levels returns a slice of Levels the hook is fired for.
+func (h *JournalHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+// Enable adds the Journal hook if journal is enabled.
+// Sets log output to ioutil.Discard so stdout isn't captured.
+func Enable() {
+	if journald.IsNotExists() {
+		logrus.Warning("Journal not available but user requests we log to it. Ignoring")
+	} else {
+		logrus.AddHook(&JournalHook{})
+		logrus.SetOutput(ioutil.Discard)
 	}
-)
+}
+
+func mapLevelToPriority(l logrus.Level) journald.Priority {
+	switch l {
+	case logrus.DebugLevel:
+		return journald.PriorityDebug
+	case logrus.InfoLevel:
+		return journald.PriorityInfo
+	case logrus.WarnLevel:
+		return journald.PriorityWarning
+	case logrus.ErrorLevel:
+		return journald.PriorityErr
+	case logrus.FatalLevel:
+		return journald.PriorityCrit
+	case logrus.PanicLevel:
+		return journald.PriorityEmerg
+	}
+	return journald.PriorityNotice
+}
 
 func stringifyOp(r rune) rune {
-	// Journal wants uppercase strings. See `validVarName`
-	// https://github.com/coreos/go-systemd/blob/ff118ad0f8d9cf99903d3391ca3a295671022cee/journal/journal.go#L137-L147
 	switch {
 	case r >= 'A' && r <= 'Z':
 		return r
-	case r >= '0' && r <= '9':
-		return r
-	case r == '_':
+	case unicode.IsDigit(r):
 		return r
 	case r >= 'a' && r <= 'z':
-		return r - 32
+		return unicode.ToUpper(r)
 	default:
 		return rune('_')
 	}
@@ -51,36 +82,8 @@ func stringifyKey(key string) string {
 func stringifyEntries(data map[string]interface{}) map[string]string {
 	entries := make(map[string]string)
 	for k, v := range data {
-
 		key := stringifyKey(k)
 		entries[key] = fmt.Sprint(v)
 	}
 	return entries
-}
-
-func (hook *JournalHook) Fire(entry *logrus.Entry) error {
-	return journal.Send(entry.Message, severityMap[entry.Level], stringifyEntries(entry.Data))
-}
-
-// `Levels()` returns a slice of `Levels` the hook is fired for.
-func (hook *JournalHook) Levels() []logrus.Level {
-	return []logrus.Level{
-		logrus.PanicLevel,
-		logrus.FatalLevel,
-		logrus.ErrorLevel,
-		logrus.WarnLevel,
-		logrus.InfoLevel,
-		logrus.DebugLevel,
-	}
-}
-
-// Adds the Journal hook if journal is enabled
-// Sets log output to ioutil.Discard so stdout isn't captured.
-func Enable() {
-	if !journal.Enabled() {
-		logrus.Warning("Journal not available but user requests we log to it. Ignoring")
-	} else {
-		logrus.AddHook(&JournalHook{})
-		logrus.SetOutput(ioutil.Discard)
-	}
 }
